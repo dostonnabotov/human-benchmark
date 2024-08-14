6157445b28a27b4709f87659e6f4468f9437254e
Doston Nabotov <dostonnabotov@gmail.com>
2024-08-14 21:44:59 +0500

    Fix "Verbal Memory"

--- Verbal Memory/script.js
+++ Verbal Memory/script.js
@@ -1,29 +1,20 @@
+const cache = [];
+
 function Verbal_Memory() {
-  const cache = [];
-  const word = document.querySelector(".word");
-  const target = document.querySelector(".verbal-memory-test");
+  const word = document.querySelector(".word").textContent;
   const seenBtn = document.querySelectorAll(".css-de05nr.e19owgy710")[0];
   const newBtn = document.querySelectorAll(".css-de05nr.e19owgy710")[1];
 
-  console.log("hey");
-  const observer = new MutationObserver((mutationsList) => {
-    for (const mutation of mutationsList) {
-      if (mutation.type === "childList") {
-        // if (word in cache) {
-        //   // seenBtn.click();
-        // } else {
-        //   cache.push(word);
-        //   // newBtn.click();
-        // }
-      }
-    }
-  });
-
-  observer.observe(target, {
-    childList: true,
-    characterData: true,
-    subtree: true,
-  });
+  if (cache.includes(word)) {
+    console.log("found");
+    seenBtn.click();
+  } else {
+    console.log("not found");
+    cache.push(word);
+    newBtn.click();
+  }
 }
 
-Verbal_Memory();
+setInterval(() => {
+  Verbal_Memory();
+}, 2000);
